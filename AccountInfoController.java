73e69415b35aa39a2a416667c5265ccac946f1fc
Jordan Boykin <48491551+jeb6965@users.noreply.github.com>
2024-11-15 14:34:14 -0500

    Initial commit for basic interface.
    fxml and controller files for a landing page and the account info page.

---
+++ AccountInfoController.java
@@ -0,0 +1,5 @@
+package com.example.project_2_331;
+
+public class AccountInfoController {
+
+}
---
+++ MainPageController.java
@@ -0,0 +1,27 @@
+package com.example.project_2_331;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainPageController {
+
+    @FXML
+    private Button browseButton;
+
+    @FXML
+    private ChoiceBox<?> searchMenuChoice;
+
+    @FXML
+    private Label searchMenuLabel;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private AnchorPane viewAccountButton;
+
+}

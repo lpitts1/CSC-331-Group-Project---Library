9f08daaaa993508cd97c1a810efca80ab62b43ca
Jordan Boykin <48491551+jeb6965@users.noreply.github.com>
2024-11-18 16:58:39 -0500

    Updated account info and main landing page.
    Updated account info and main landing page. BookTesting class is currently being used to create a book object that works with the table and possibly a library inventory. Main class is the driver for opening the landing page.

--- AccountInfoController.java
+++ AccountInfoController.java
@@ -1,5 +1,41 @@
 package com.example.project_2_331;
 
-public class AccountInfoController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ResourceBundle;
+
+public class AccountInfoController implements Initializable {
+    @FXML private TableView<BookTesting> checkoutTable;
+    @FXML private TableColumn<BookTesting, String> titleColumn;
+    @FXML private TableColumn<BookTesting, String> authorColumn;
+    @FXML private TableColumn<BookTesting, String> genreColumn;
+    @FXML private TableColumn<BookTesting, LocalDate> checkoutDateColumn;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Initializes columns of the table for each book attribute
+        titleColumn.setCellValueFactory(new PropertyValueFactory<BookTesting, String>("bookName"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<BookTesting, String>("authorName"));
+        genreColumn.setCellValueFactory(new PropertyValueFactory<BookTesting, String>("genre"));
+        checkoutDateColumn.setCellValueFactory(new PropertyValueFactory<BookTesting, LocalDate>("checkOutDate"));
+        // Sets the values
+        checkoutTable.setItems(getBook());
+    }
+    public ObservableList<BookTesting> getBook(){
+        // Observable list for usage with the TableView object
+        ObservableList<BookTesting> books = FXCollections.observableArrayList();
+        // Placeholder book objects for testing
+        books.add(new BookTesting("Dictionary", "Miriam-Webster","Non-Fiction", LocalDate.of(2024, Month.NOVEMBER, 18)));
+        books.add(new BookTesting("Java How to Program Early Objects", "Deitel","Non fiction", LocalDate.of(2024, Month.NOVEMBER, 18)));
+        // Returns the observable list
+        return books;
+    }
 }
---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.example.project_2_331;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception{
+        // Loads the main page
+        Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("accountInfo");
+        stage.setScene(scene);
+        stage.show();
+    }
+}

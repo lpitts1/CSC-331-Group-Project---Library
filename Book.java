92c290b36795abd0b9e71c02c8a6ccf4f03eff8a
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-11-23 21:17:08 -0500

    Update Book.java
    Added the comments for the book classes

--- Book.java
+++ Book.java
@@ -2,7 +2,9 @@ package com.example.library;
 
 
 import java.time.LocalDate;
-
+/**
+ * Represents a book in the library with the details of the book
+ */
 public class Book {
     public String bookName;
     public String authorName;
@@ -11,8 +13,16 @@ public class Book {
     public int bookQtyCopy;
     public int sNo;
     public LocalDate checkOutDate;
-
-    // Constructor for library usage?
+    
+    /**
+     * Constructs the Book object for the library
+     *
+     * @param bookName
+     * @param authorName
+     * @param genre
+     * @param bookQty
+     */
+    
     public Book(String bookName, String authorName, String genre, int bookQty)
     {
         this.bookName = bookName;
@@ -22,6 +32,14 @@ public class Book {
     }
 
     // Constructor for checked out com.example.librarygroupproject.books usage?
+    /**
+     * Constructs the Book object for the checked-out books
+     *
+     * @param bookName
+     * @param authorName
+     * @param genre
+     * @param checkOutDate
+     */
     public Book(String bookName, String authorName, String genre, LocalDate checkOutDate)
     {
         this.bookName = bookName;
@@ -30,31 +48,57 @@ public class Book {
         this.checkOutDate = checkOutDate;
     }
 
-
+    /**
+     * Getter for the bookName
+     * @return bookName
+     */
     public String getBookName() {
         return bookName;
     }
-
+    /**
+     * Getter for the authorName
+     * @return authorName
+     */
+    
     public String getAuthorName() {
         return authorName;
     }
-
+    
+    /**
+     * Getter for the Genre
+     * @return Genre
+     */
     public String getGenre() {
         return genre;
     }
-
+    
+    /**
+     * Getter for the Book Quantity
+     * @return Book Quantity
+     */
     public int getBookQty() {
 
         return bookQty;
     }
-
+    
+    /**
+     * Getter for the checkOutDate
+     * @return checkOutDate
+     */
     public LocalDate getCheckOutDate() {
         return checkOutDate;
     }
-
+    
+    /**
+     * Decreases the book quantity by one if the book is checked out
+     */
     public void checkOut(){
         this.bookQty = bookQty -1;
     }
+    
+    /**
+     * Increases the book quantity by one if the book is checked in
+     */
     public void checkIn(){
         this.bookQty = bookQty +1;
     }

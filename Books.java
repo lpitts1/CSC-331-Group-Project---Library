4d9e238ac0baeaa590b2dac999aa58c658bc0bf3
Rosebud2003 <140856971+Rosebud2003@users.noreply.github.com>
2024-11-23 21:51:32 -0500

    Update Books.java
    Comments added to the books class

--- Books.java
+++ Books.java
@@ -1,53 +1,64 @@
 package com.example.library;
 
 import java.util.Scanner;
-
+/**
+ * This creates the books class
+ */
 public class Books {
 
-    Book allBooks[] = new Book[25];
-    public static int count;
+    Book allBooks[] = new Book[25];  //Array of all the books in the library
+    public static int count; //Total in library
 
     Scanner input = new Scanner(System.in);
 
 
+    /**
+     * Compares the two books by the name
+     * @param b1, first book to compare
+     * @param b2, second book to compare
+     * @return 0 if books have the same name, 1 if not
+     */
     public int compareBooks(Book b1, Book b2){
 
         if(b1.bookName.equalsIgnoreCase(b2.bookName)){
-            System.out.println("This com.example.librarygroupproject.book already exists");
-
-            return 0;
+            System.out.println("This com.example.librarygroupproject.book already exists"); //Checks if the books are the same
+            return 0; //Same name
         }
-
-        return 1;
+        return 1; //Different name
     }
 
+    /**
+     * Adds a new book to the library if the book does not already exist
+     * @param b, book added
+     */
     public void newBook(Book b){
-
         for (int i = 0; i<count; i++){
-            if(this.compareBooks(b,this.allBooks[i])==0){
-                return;
+            if(this.compareBooks(b,this.allBooks[i])==0){  //Checking for duplicates
+                return; //Exiting if the book exists or there is a duplicate
             }
         }
+
+        //Checks if the library is full or not
         if (count < 25){
-
-
-            allBooks[count] = b;
-            count++;
+            allBooks[count] = b; //Add the book to the array
+            count++;  //Add one to the count
         }
         else{
             System.out.println("com.example.librarygroupproject.Library is Full");
         }
     }
-
+    
+    /**
+     * Searches by author and prompts for the author name
+     */
     public void searchAuthor(){
         System.out.println("Enter Author Name:");
         String authorName = input.nextLine();
-
-        int flag = 0;
-
+        int flag = 0;  //Tracks whether any books by the inputted author are found in the library
         System.out.println(
                 "S.No\t\tName\t\tAuthor\t\tAvailable Qty\t\tTotal Qty");
-
+        
+        //Loop through all the books and print the detail for the matching authors
         for(int i = 0; i<count; i++){
             if (authorName.equalsIgnoreCase(allBooks[i].authorName)){
                 System.out.println(allBooks[i].bookName +"\t\t"
@@ -58,27 +69,27 @@ public class Books {
                 flag++;
             }
         }
+        //Message for if no books by the author are found
         if (flag == 0){
             System.out.println("No com.example.librarygroupproject.books from that author found.");
         }
     }
 
-    public void searchBySno()
-    {
-
+    /**
+     * Searches for a book by the serial number
+     */
+    public void searchBySno(){
         // Display message
         System.out.println(
                 "\t\t\t\tSEARCH BY SERIAL NUMBER\n");
-
         // Class data com.example.librarygroupproject.members
         int sNo;
         System.out.println("Enter Serial No of Book:");
         sNo = input.nextInt();
-
         int flag = 0;
         System.out.println(
                 "S.No\t\tName\t\tAuthor\t\tAvailable Qty\t\tTotal Qty");
-
+        //Loop through for all matching serial number books
         for (int i = 0; i < count; i++) {
             if (sNo == allBooks[i].sNo) {
                 System.out.println(
@@ -91,19 +102,21 @@ public class Books {
                 return;
             }
         }
+         //Display if no matching book is found
         if (flag == 0)
             System.out.println("No Book for Serial No "
                     + sNo + " Found.");
     }
-
-
+    
+    /**
+     * Displays all the books in the library
+     */
     public void showAllBooks() {
         System.out.println("\t\t\t\tSHOWING ALL BOOKS\n");
         System.out.println(
                 "S.No\t\tName\t\tAuthor\t\tAvailable Qty\t\tTotal Qty");
-
+        //Looping through and displaying all the books
         for (int i = 0; i < count; i++) {
-
             System.out.println(
                     allBooks[i].sNo + "\t\t"
                             + allBooks[i].bookName + "\t\t"
@@ -114,30 +127,31 @@ public class Books {
         }
     }
 
+    /**
+     * Updates the qauntity of a book
+     */
     public void upgradeBookQty(){
         System.out.println(
                 "\t\t\t\tUPGRADE QUANTITY OF A BOOK\n");
         System.out.println("Enter Serial No of Book");
-
         int sNo = input.nextInt();
-
+        //Search for the book and update the quanitity of the book
         for (int i = 0; i < count; i++) {
-
             if (sNo == allBooks[i].sNo) {
-
                 // Display message
                 System.out.println(
                         "Enter No of Books to be Added:");
-
                 int addingQty = input.nextInt();
                 allBooks[i].bookQty += addingQty;
                 allBooks[i].bookQtyCopy += addingQty;
-
                 return;
             }
         }
     }
 
+    /**
+     * Method to display the menu
+     */
     public void dispMenu(){
         // Displaying menu
         System.out.println(
@@ -157,7 +171,12 @@ public class Books {
                 "-------------------------------------------------------------------------------------------------------");
 
     }
-
+    
+    /**
+     * Checks if a book is a by its name and the quantity
+     * @param b, name of book
+     * @return index number if found, -1 otherwise
+     */
     public int isAvailable(String b){
         for(int i= 0; i<count; i++){
             if(b.equalsIgnoreCase(allBooks[i].bookName)){
@@ -174,21 +193,33 @@ public class Books {
         return -1;
     }
 
+    /**
+     * Method for checking out a book from the libraru
+     * Method also reduces the number of available books by one
+     * @return null or the book that is checked out
+     */
     public Book checkOut(){
         System.out.println("Enter name of book to be checked out:");
-
+        //User input
         String name = input.nextLine();
-
         int bookIndex = isAvailable(name);
-
+        //If the book is available, the quantity is decreased
         if (bookIndex!= -1){
             allBooks[bookIndex].bookQtyCopy--;
             return allBooks[bookIndex];
         }
+        //Null if the book is not available
         return null;
     }
 
+    /**
+     * Checks in the book that has been borrowed
+     * Book quantity is increased if the book is returned
+     * @param b, the book that has been checked out/is being returned
+     */
     public void checkIn(Book b){
+        // Loop through the list of book and find the match, if there is a match
+        // Add one and increase the book quantity
         for (int i = 0; i < count; i++){
             if (b.equals(allBooks[i])){
                 allBooks[i].bookQtyCopy++;
